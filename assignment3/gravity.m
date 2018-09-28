ae70f02d2938743433df7626f5ea1a6d30dc4bde
Thomas McCredie <thomasmccredie@gmail.com>
2018-09-28 19:01:35 +1000

    added function gravity

---
+++ assignment3/gravity.m
@@ -0,0 +1,31 @@
+% AERO3560 - Flight Mechanics 1 - Assignment 3 2018
+% Author SID: 460306678
+% Function Name: gravity
+%
+% Function Description:
+% Determines the force due to gravity and converts it to the body frame
+%
+% Inputs:
+%   Input: - mass of aircraft (mass)
+%          - attitude in quaternions (q0,q1,q2,q3)
+%
+% Outputs:
+%   Output: - body force components (Fgx,Fgy,Fgz)
+
+function [Fgx, Fgy, Fgz] = gravity(mass,q0,q1,q2,q3)
+
+    % Calculate weight force
+    g = 9.81;
+    F = mass*g;
+    
+    % Create transformation matrix from Earth to body
+    Cbe = rotate321quat([q0,q1,q2,q3]);
+
+    % Transform force
+    Fg = Cbe*F;
+    Fgx = Fg(1);
+    Fgy = Fg(2);
+    Fgz = Fg(3);
+
+
+end
