6a9291e2b5c729f1c99d206f0c7ff07946648161
RyanJeffreson <rrcjeffreson@gmail.com>
2018-09-28 16:51:39 +1000

    updatded euler2quat

--- assignment3/euler2quat.m
+++ assignment3/euler2quat.m
@@ -1,5 +1,6 @@
 % AERO3560 - Flight Mechanics 1 - Assignment 1 2018
 % Author SID: 460369684
+% With Special Thanks To: 460306678
 % Function Name: euler2quat
 %
 % Function Description:
@@ -13,22 +14,21 @@
 %   quaternion: - matrix of quaternion components [q0; q1; q2; q3]
 
 function quaternion = euler2quat(eulAngles)
-
+    
     % Unpack Euler angles matrix
     phi = eulAngles(1,:);
     theta = eulAngles(2,:);
     psi = eulAngles(3,:);
     
-    % Divide by 2
-    phi = phi/2;
-    theta = theta/2;
-    psi = psi/2;
-    
     % Calculate quaternion components
-    q0 = cos(psi).*cos(theta).*cos(phi) + sin(psi).*sin(theta).*sin(phi);
-    q1 = cos(psi).*cos(theta).*sin(phi) - sin(psi).*sin(theta).*cos(phi);
-    q2 = cos(psi).*sin(theta).*cos(phi) + sin(psi).*cos(theta).*sin(phi);
-    q3 = -cos(psi).*sin(theta).*sin(phi) + sin(psi).*cos(theta).*cos(phi);
+    q0 = cos(psi/2).*cos(theta/2).*cos(phi/2) + ...
+        sin(psi/2).*sin(theta/2).*sin(phi/2);
+    q1 = cos(psi/2).*cos(theta/2).*sin(phi/2) - ...
+        sin(psi/2).*sin(theta/2).*cos(phi/2);
+    q2 = cos(psi/2).*sin(theta/2).*cos(phi/2) + ...
+        sin(psi/2).*cos(theta/2).*sin(phi/2);
+    q3 = -cos(psi/2).*sin(theta/2).*sin(phi/2) + ...
+        sin(psi/2).*cos(theta/2).*cos(phi/2);
     
     % Return quaternion vector
     quaternion = [q0; q1; q2; q3];
