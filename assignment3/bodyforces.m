a55a7c8be9217af0692ae59518a971c9693b2d99
TheHolyCow <shaka_chu@hotmail.com>
2018-10-01 15:55:17 +1000

    add bodyforces func

---
+++ assignment3/bodyforces.m
@@ -0,0 +1,18 @@
+% AERO3560 - Flight Mechanics 1 - Assignment 3 2018
+% Author SID: 460373315
+% Function Name: bodyforces
+%
+% F_BODY = BODYFORCES()
+%
+% Function Description:
+% Returns the forces acting on the aircraft in the body axes
+%
+% Inputs:
+%
+% Outputs:
+%   F_body = matrix of the combined body forces [Fx; Fy; Fz]
+
+function F_body = bodyforces()
+
+    
+end
