cb2e8d5331f4ec2cb1488e6671aafee891d982ec
RyanJeffreson <rrcjeffreson@gmail.com>
2018-10-03 17:00:07 +1000

    runge kutta done i think

--- assignment3/rungeKutta4.m
+++ assignment3/rungeKutta4.m
@@ -1,22 +1,38 @@
-function [X] = rungeKutta4(Params,X0,U0,dt)
+function [X_new] = rungeKutta4(Params,X0,U,dt)
     
-    X_dot_1 = staterates(X_0, U_0, Params)
+    % State rate at start of step
+    X_dot_1 = staterates(Params, X0, U);
     
-    An = X_dot_1*dt/2;
+    % Incriment 
+    An = X_dot_1*dt;
     
-    X_2 = X_0 + An
+    % First state at middle of step
+    X_2 = X_0 + An/2;
     
+    % First state rate at middle of step
+    X_dot_2 = staterates(Params, X_2, U);
     
+    % Improved incriment 
+    Bn = X_dot_2*dt;
     
-    X_dot_2 = staterates(X_2, U_0, Params,)
-
-     Ft, F_body, M_body, Fgx, Fgy, Fgz
-
-    % Step 1
-    An = Xdot*dt;
+    % Second state at middle of step
+    X_3 = X_0 + Bn/2;
     
+    % Second state rate at middle of step
+    X_dot_3 = staterates(Params, X_3, U);
     
+    % Improved incriment
+    Cn = X_dot_3*dt;
     
+    % State at end of step
+    X_4 = X_0 + Cn;
     
-
+    % State rate at end of step
+    X_dot_4 = staterates(Params, X_4, U);
+    
+    % Improved incriment
+    Dn = X_dot_4*dt;
+    
+    % Evaluate new state
+    X_new = X_0 + (1/6)*(An + 2*Bn + 2*Cn + Dn);
 end
