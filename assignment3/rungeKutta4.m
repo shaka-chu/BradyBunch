4ec9b4cd1eb9daea022561a9b694d2d8403b31e6
RyanJeffreson <rrcjeffreson@gmail.com>
2018-10-03 21:30:11 +1000

    some funciton headers updated

--- assignment3/rungeKutta4.m
+++ assignment3/rungeKutta4.m
@@ -1,3 +1,51 @@
+% AERO3560 - Flight Mechanics 1 - Assignment 3 2018
+% Author SID: 460369684
+% Function Name: rungeKutta4
+%
+% Function Description:
+%   Performs numerical integration on the aircraft state at time t_k to
+%   obtain the state at t_k+1. Employs a 4th order Runge-Kutta integration
+%   scheme
+%
+% Inputs:
+%   Params: Struct containing all characteristics of the aircraft
+%   X0:     Vector containing the aircraft state at the beginning of the 
+%           time step. The order is:
+%               - u   = X(1)    (m/s)
+%               - v   = X(2)    (m/s)
+%               - w   = X(3)    (m/s)
+%               - p   = X(4)    (rad/s)
+%               - q   = X(5)    (rad/s)
+%               - r   = X(6)    (rad/s)
+%               - q0  = X(7)    -
+%               - q1  = X(8)    -
+%               - q2  = X(9)    -
+%               - q3  = X(10)   -
+%               - x   = X(11)   (m)
+%               - y   = X(12)   (m)
+%               - z   = X(13)   (m)
+%   U:      Vector containing all aircraft control settings. The order is:
+%               - delta_t = U(1)    -
+%               - delta_e = U(2)    (rad)
+%               - delta_a = U(3)    (rad)
+%               - delta_r = U(4)    (rad)
+%   dt:     Timestep size (s)
+%
+% Outputs:
+%   X_new:  Aircraft state at 
+%
+% Other m-files required:
+%   staterates, calculateForces.m, aeroangles.m, flowproperties.m, 
+%   gravity.m, windforces.m, bodyforces.m, gravForces.m, propforce.m
+%
+% Subfunctions:
+%   staterates.m, calculateForces, aeroangles, flowproperties, gravity,
+%   windforces, bodyforces, gravForces, propforce
+%
+% MAT-files required: none
+%
+% TODO: none
+
 function [X_new] = rungeKutta4(Params,X0,U,dt)
     
     % State rate at start of step
