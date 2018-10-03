92c40f56dd855f3203d101724963979a215c76dd
jamesofdoom <james1986k@hotmail.com>
2018-10-03 23:48:38 +1000

    alphadot and betadot

--- assignment3/alphabeta_dot.m
+++ assignment3/alphabeta_dot.m
@@ -1,6 +1,6 @@
 % AERO3560 - Flight Mechanics 1 - Assignment 3 2018
 % Author SID: 460398189
-% Alphabeta_dot
+% AlphaBeta_dot
 %
 %
 %
@@ -8,15 +8,14 @@
 % Inputs: X_dot, X arrays (column vectors)
 %
 % Outputs:
-% alpha_dot : Single value for alpha_dot using trhe following 
-% alphadot = (wdot*u-w*udot)/(u^2)
-% 
-% [udot;vdot;wdot;pdot;qdot;rdot;q0dot;q1dot;q2dot;q3dot;position(1);position(2);position(3)]
+% [alpha_dot, beta_dot]
 
-function [alpha_dot] = alphabeta_dot(Xdot,X)
+function [alpha_dot, beta_dot] = alphabeta_dot(Xdot,X)
 
         % Calcualte alpha_dot 
-        alpha_dot = (Xdot(3)*X(3) - X(1)*Xdot(1))/X(3);
-
+        alpha_dot = (Xdot(3)*X(1) - X(3)*Xdot(1))/X(1);
+        
+        % Calculate beta_dot MUST CHANGE ------------
+        beta_dot = (Xdot(3)*X(2) - X(2)*Xdot(2))/X(3);
 end
 
