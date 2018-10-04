bfa41682173900807a04c391d6dd6766b408b9cf
TheHolyCow <shaka_chu@hotmail.com>
2018-10-04 19:16:06 +1000

    add more shit to test

--- assignment3/TOM_IS_GREAT.m
+++ assignment3/TOM_IS_GREAT.m
@@ -20,3 +20,8 @@ load ICs_PC9_nominalCG1_180Kn_1000ft
 
 CG1_180Kn_X = X0;
 CG1_180Kn_U = U0;
+
+isU0Right1 = trim(CG2_100Kn_X);
+isU0Right2 = trim(CG2_180Kn_X);
+isU0Right3 = trim(CG1_100Kn_X);
+isU0Right4 = trim(CG1_180Kn_X);
