362bd07aae0f4ce6e24d5831aeecb798012c6edd
TheHolyCow <shaka_chu@hotmail.com>
2018-10-04 19:14:38 +1000

    add more shit to test

--- assignment3/TOM_IS_GREAT.m
+++ assignment3/TOM_IS_GREAT.m
@@ -1,1 +1,22 @@
 % Test
+
+% load a bunch of shit
+load ICs_PC9_CG2_100Kn_1000ft
+
+CG2_100Kn_X = X0;
+CG2_100Kn_U = U0;
+
+load ICs_PC9_CG2_180Kn_1000ft
+
+CG2_180Kn_X = X0;
+CG2_180Kn_U = U0;
+
+load ICs_PC9_nominalCG1_100Kn_1000ft
+
+CG1_100Kn_X = X0;
+CG1_100Kn_U = U0;
+
+load ICs_PC9_nominalCG1_180Kn_1000ft
+
+CG1_180Kn_X = X0;
+CG1_180Kn_U = U0;
