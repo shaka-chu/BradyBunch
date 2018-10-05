62d532139540aa4f665dfb7c40247ff8beb6f663
Thomas McCredie <thomasmccredie@gmail.com>
2018-10-05 16:54:31 +1000

    added main functionality

---
+++ assignment3/controls.m
@@ -0,0 +1,35 @@
+% AERO3560 - Flight Mechanics 1 - Assignment 3 2018
+% Author SID: 460306678
+% Function Name: controls
+%
+% Function Description:
+%   Determines the control input vector for a manoeuvre
+%
+% Inputs:
+%   U:          Vector containing all aircraft control settings. The order is:
+%                   - delta_t = U(1)    -
+%                   - delta_e = U(2)    (rad)
+%                   - delta_a = U(3)    (rad)
+%                   - delta_r = U(4)    (rad)
+%   Time:       Time simulation vector
+%   CurrentTime:Current time of the simutlation 
+%
+% Outputs:
+%   U_manoeuvre:  Aircraft state at end of time step. Same order and units as 
+%           'X0', listed above
+%
+% Other m-files required:
+%   None
+%
+% Subfunctions:
+%   None
+%
+% MAT-files required: none
+%
+% TODO: none
+
+function U_manoeurve = controls(U_trimmed, time, currentTime)
+
+    % Set new vector
+    U_manoeurve = U_trimmed;
+end
